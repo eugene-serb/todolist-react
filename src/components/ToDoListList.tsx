e9b61f84b550d1a4e2f1944d7e35d4c5e4026d3c
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-08-27 16:32:58 +0300

    - updated base css. - changed tabs width from 4 to 2. - updated links in header and head meta. - added link to bugs tracker in package. - updated packages. - updated version. - rebuild.

--- src/components/ToDoListList.tsx
+++ src/components/ToDoListList.tsx
@@ -3,33 +3,33 @@ import ToDoListTask from './ToDoListTask';
 import Task from '../models/Task';
 
 interface IToDoListList {
-    tasks: Task[],
-    markComplete: Function,
-    markImportant: Function,
-    deleteTask: Function,
+  tasks: Task[],
+  markComplete: Function,
+  markImportant: Function,
+  deleteTask: Function,
 };
 
-function ToDoListList({tasks, markComplete, markImportant, deleteTask}: IToDoListList) {
-    return (
-        <div className="todoList-list">
-            <h3>Tasks:</h3>
-            <ul className="todoList-wrapper">
-                {
-                    tasks.map(task => {
-                        return (
-                            <ToDoListTask
-                                key={task.id as number}
-                                task={task as Task}
-                                markComplete={markComplete as Function}
-                                markImportant={markImportant as Function}
-                                deleteTask={deleteTask as Function}
-                            />
-                        );
-                    })
-                }
-            </ul>
-        </div>
-    );
+function ToDoListList({ tasks, markComplete, markImportant, deleteTask }: IToDoListList) {
+  return (
+    <div className="todoList-list">
+      <h3>Tasks:</h3>
+      <ul className="todoList-wrapper">
+        {
+          tasks.map(task => {
+            return (
+              <ToDoListTask
+                key={task.id as number}
+                task={task as Task}
+                markComplete={markComplete as Function}
+                markImportant={markImportant as Function}
+                deleteTask={deleteTask as Function}
+              />
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
 };
 
 export default ToDoListList;
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import ToDoList from './components/ToDoList';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <ToDoList />
-    </React.StrictMode>
+  <React.StrictMode>
+    <ToDoList />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
